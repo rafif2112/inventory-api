498805177afc2d52fd6fec14d20bb0e151496f5e
Julianvin <delvinaping10@gmail.com>
2025-08-05 22:33:31 +0700

    feat: menambahkn 2 endpoint (major dan item)

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ItemController;
+use App\Http\Controllers\MajorController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
@@ -29,4 +31,6 @@ Route::middleware('auth:api')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
 
     Route::apiResource('/student', StudentController::class);
+    Route::apiResource('/item', ItemController::class);
+    Route::apiResource('/major', MajorController::class);
 });
