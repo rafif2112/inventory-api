638c1dc071a7223ea2a1f4ed3c77acf19ef83cef
Julianvin <delvinaping10@gmail.com>
2025-08-08 19:05:52 +0700

    fix: dd forget to delete

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -81,7 +81,6 @@ class ItemController extends Controller
         try {
             $item->whereId($item->id)->update($validatedData);
 
-            dd($item);
             DB::commit();
             return response()->json([
                 'status' => 200,
